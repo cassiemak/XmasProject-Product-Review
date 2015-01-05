39800d297debe775af4dbb09ec8f433b0960f89b
cassiemak <cassie.mak@gmail.com>
2015-01-05 10:46:00 +0800

    create new product works product create and product new done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,28 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    product = Product.new(product_param)
+
+    if product.save 
+      redirect_to products_path
+    else
+      flash[:message] = product.errors.messages
+      redirect_to :back
+    end
+  end
+
   def show
     @products = Product.find(params[:id])
   end
 
   def index
     @products = Product.all
+  end
+
+  private
+  def product_param
+    params.require(:product).permit(:product_name, :brand, :price, :found_at, :description)
   end
 
 end
